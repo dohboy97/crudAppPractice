111e0faa53534c77b3410d9702f9f55773d2ccda
Connor <connor.doherty97@gmail.com>
2022-08-08 16:03:02 -0400

    basic node server working and responding w html file to localhost requests

--- refresher/server.js
+++ refresher/server.js
@@ -1,1 +1,15 @@
 console.log('May Node be with you')
+
+const express = require ('express');
+
+
+const app = express();
+
+
+app.get('/',(req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3000, function(){
+    console.log('listening on server 3000')
+})

d344c77c4198cc4773df0479d876654509b6d979
Connor <connor.doherty97@gmail.com>
2022-08-08 17:31:45 -0400

    ready for mongo db

--- refresher/server.js
+++ refresher/server.js
@@ -1,13 +1,20 @@
-console.log('May Node be with you')
+
 
 const express = require ('express');
 
-
+const bodyParser =require('body-parser')
 const app = express();
+const MongoClient = require ('mongodb').MongoClient
+app.use(bodyParser.urlencoded({extended:true}))
 
 
 app.get('/',(req, res)=>{
-    res.sendFile(__dirname + '/index.html')
+     res.sendFile(__dirname + '/index.html')
+ 
+})
+
+app.post('/quotes', (req,res)=>{
+    console.log(req.body)
 })
 
 app.listen(3000, function(){

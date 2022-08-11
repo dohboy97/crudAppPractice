d2a9933a84af89bc50e45232d5743e1842b0f325
Connor <connor.doherty97@gmail.com>
2022-08-11 15:29:49 -0400

    js primed for express delete

--- refresher/public/main.js
+++ refresher/public/main.js
@@ -17,3 +17,21 @@ update.addEventListener('click', _ =>{
         console.log(response)
     })
 })
+
+const deleteButton = document.querySelector('delete-button')
+
+deleteButton.addEventListener('click', _ => {
+    fetch('/quotes', {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            name:'Darth Vader'
+        })
+    })
+    .then(res=>{
+        if(res.ok) return res.json()
+    })
+    .then(data =>{
+        window.location.reload()
+    })
+})
